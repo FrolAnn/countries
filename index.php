d5726c1cbc5c025f2055bfd8bb264a29f396bcd1
Ann <frolann23@gmail.com>
2018-08-10 14:55:05 +0300

    Добавлен скрипт загрузки страницы

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once $_SERVER ['DOCUMENT_ROOT'] . '/header.html';
+include_once $_SERVER ['DOCUMENT_ROOT'] . '/table.html';
+include_once $_SERVER ['DOCUMENT_ROOT'] . '/footer.html';
+
+?>
+
